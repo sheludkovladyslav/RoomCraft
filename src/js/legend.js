35161b90d15d473f807c296227d58d8f335d2f83
AnnaZuzkova <famzu2018@gmail.com>
2025-07-28 17:38:34 +0300

    animation-fix

--- src/js/legend.js
+++ src/js/legend.js
@@ -1,25 +1,38 @@
 const showButton = document.querySelector('.show');
-const legend = document.querySelector('.legend');
+const legend = document.querySelector('.legend__info');
+const closeButton = document.querySelector('.close-legend');
 
 const legendOpen = () => {
-  legend.style.display = 'block';
-  requestAnimationFrame(() => {
-    legend.style.opacity = '1';
-    legend.style.transform = 'translateY(0)';
-  });
-  showButton.style.display = 'none';
+  if (window.innerWidth > 767) {
+    legend.style.display = 'flex';
+    showButton.style.display = 'none';
+
+    requestAnimationFrame(() => {
+      legend.style.opacity = '1';
+      legend.style.transform = 'translateY(0)';
+    });
+    setTimeout(() => {
+      closeButton.style.display = 'block';
+      requestAnimationFrame(() => {
+        closeButton.style.opacity = '1';
+      });
+    }, 400);
+  }
 };
 
 const legendClouse = () => {
   if (window.innerWidth > 767) {
     legend.style.opacity = '0';
     legend.style.transform = 'translateY(-50px)';
+    closeButton.style.opacity = '0';
+
     setTimeout(() => {
       legend.style.display = 'none';
+      closeButton.style.display = 'none';
       showButton.style.display = 'flex';
     }, 300);
   }
 };
 
 showButton.addEventListener('click', legendOpen);
-legend.addEventListener('click', legendClouse);
+closeButton.addEventListener('click', legendClouse);

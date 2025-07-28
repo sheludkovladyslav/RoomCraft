57c2033ef78e7b1b58f82c1f1e1f0549086fccfe
vladyslavsheludko <vladsh2103@gmail.com>
2025-07-28 13:26:29 -0400

    fixed all menu bugs, added animations for clicks, fixed paddings and width problems

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,41 +1,53 @@
 import furnitureList from './furnitures.js';
 
-
-const menuCards = document.getElementById('menuCards')
+const menuCards = document.getElementById('menuCards');
 const menuFilter = document.getElementById('menuFilter');
-
 const categoryMap = {
-  'декорації': 'decoration',
-  'меблі': 'furniture',
-  'електроніка': 'electronics',
+  декорації: 'decoration',
+  меблі: 'furniture',
+  електроніка: 'electronics',
   'ін.': 'all',
 };
 
 const renderCards = (category = 'all') => {
-    menuCards.innerHTML = furnitureList
+  menuCards.innerHTML = furnitureList
     .filter(item => category === 'all' || item.category === category)
-    .map((item) => {
-        return `
-            <article class="menu-cards-card">
+    .map(item => {
+      return `
+            <li class="menu-cards-card">
           <button class="furniture__spawn" data-spawn="${item.key}">
          <div class="menu-cards-card__img"></div>
-        <h3 class="menu-cards-card__title">Name: ${item.name}</h3>
-    <span class="menu-cards-card__category">Category: ${item.category}</span>
+        <h3 class="menu-cards-card__title">${item.name}</h3>
+    <p class="menu-cards-card__category">Категорія: ${item.category}</p>
               </button>
-</article>
-        `}).join('')
-}
+</li>
+        `;
+    })
+    .join('');
+};
 
-menuFilter.addEventListener('click', (e) => {
-    const li = e.target.closest('.menu-filter__list')
+menuFilter.addEventListener('click', e => {
+  const li = e.target.closest('.menu-filter__list');
 
-     document.querySelectorAll('.menu-filter__list').forEach((el) => el.classList.remove('active'))
+  document
+    .querySelectorAll('.menu-filter__list')
+    .forEach(el => el.classList.remove('active'));
 
-    li.classList.add('active')
+  li.classList.add('active');
 
-    const category = categoryMap[li.textContent.trim().toLowerCase()] || 'all'
-    renderCards(category)
-})
+  const category = categoryMap[li.textContent.trim().toLowerCase()] || 'all';
+  renderCards(category);
+});
 
-renderCards()
+renderCards();
 
+menuCards.addEventListener('click', e => {
+  const card = e.target.closest('.menu-cards-card');
+  if (!card) return;
+
+  document
+    .querySelectorAll('.menu-cards-card')
+    .forEach(c => c.classList.remove('active'));
+
+  card.classList.add('active');
+});
